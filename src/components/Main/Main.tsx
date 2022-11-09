d9adbd9e47843e36a1ba785cf7453530e1ee19a9
Pabllot07 <pabllot07@outlook.com>
2022-11-09 12:17:57 -0300

    hero

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -4,54 +4,53 @@ import './styles.css'
 
 const Main = () => {
   return (
-    <>
-        <div>Main</div>
-        <Link to='/lesson1'>
-            <button>LESSON ONE</button>
+    <div className='mainWrapper'>
+        <Link to='/lesson1' style={{ textDecoration: 'none' }}>
+            <p>LESSON ONE</p>
         </Link>
-        <Link to='/lesson2'>
-            <button>LESSON TWO</button>
+        <Link to='/lesson2' style={{ textDecoration: 'none' }} >
+            <p>LESSON TWO</p>
         </Link>
-        <Link to='/lesson3'>
-            <button>LESSON 3</button>
+        <Link to='/lesson3' style={{ textDecoration: 'none' }} >
+            <p>LESSON 3</p>
         </Link>
-        <Link to='/lesson4'>
-            <button>LESSON 4</button>
+        <Link to='/lesson4' style={{ textDecoration: 'none' }} >
+            <p>LESSON 4</p>
         </Link>
-        <Link to='/lesson5'>
-            <button>LESSON 5</button>
+        <Link to='/lesson5' style={{ textDecoration: 'none' }} >
+            <p>LESSON 5</p>
         </Link>
-        <Link to='/lesson6'>
-            <button>LESSON 6</button>
+        <Link to='/lesson6' style={{ textDecoration: 'none' }} >
+            <p>LESSON 6</p>
         </Link>
-        <Link to='/lesson7'>
-            <button>LESSON 7</button>
+        <Link to='/lesson7' style={{ textDecoration: 'none' }} >
+            <p>LESSON 7</p>
         </Link>
-        <Link to='/lesson8'>
-            <button>LESSON 8</button>
+        <Link to='/lesson8' style={{ textDecoration: 'none' }} >
+            <p>LESSON 8</p>
         </Link>
-        <Link to='/lesson9'>
-            <button>LESSON 9</button>
+        <Link to='/lesson9' style={{ textDecoration: 'none' }} >
+            <p>LESSON 9</p>
         </Link>
-        <Link to='/lesson10'>
-            <button>LESSON 10</button>
+        <Link to='/lesson10 ' style={{ textDecoration: 'none' }}>
+            <p>LESSON 10</p>
         </Link>
-        <Link to='/lesson11'>
-            <button>LESSON 11</button>
+        <Link to='/lesson11 ' style={{ textDecoration: 'none' }}>
+            <p>LESSON 11</p>
         </Link>
-        <Link to='/lesson12'>
-            <button>LESSON 12</button>
+        <Link to='/lesson12 ' style={{ textDecoration: 'none' }}>
+            <p>LESSON 12</p>
         </Link>
-        <Link to='/lesson13'>
-            <button>LESSON 13</button>
+        <Link to='/lesson13 ' style={{ textDecoration: 'none' }}>
+            <p>LESSON 13</p>
         </Link>
-        <Link to='/lesson14'>
-            <button>LESSON 14</button>
+        <Link to='/lesson14 ' style={{ textDecoration: 'none' }}>
+            <p>LESSON 14</p>
         </Link>
-        <Link to='/lesson15'>
-            <button>LESSON 15</button>
+        <Link to='/lesson15 ' style={{ textDecoration: 'none' }}>
+            <p>LESSON 15</p>
         </Link>
-    </>
+    </div>
   )
 }
 
