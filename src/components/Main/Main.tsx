03a7d77bc52dd5969593fdb3c3b23d9ad9defd15
Pabllot07 <pabllot07@outlook.com>
2023-01-15 12:58:33 -0300

    new style

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -2,17 +2,19 @@ import { Link } from 'react-router-dom'
 import Navbar from '../Navbar/Navbar'
 import './styles.css'
 
-const lessons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
+const lessons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
 
 const Main = () => {
   return (
     <div className='mainWrapper'>
         <Navbar />
+        <div className='LessonContainer'>
         {lessons.map((item:number) => (
-            <Link to={`/lesson${item}`} style={{ textDecoration: 'none' }}>
-                <p>Lesson {item}</p>
+          <Link to={`/lesson${item}`} style={{ textDecoration: 'none' }}>
+                <p>{item}</p>
             </Link>
         ))}
+        </div>
     </div>
   )
 }
