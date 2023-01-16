757d9eff925d2287c10575c2ef86810947c79f21
Pabllot07 <pabllot07@outlook.com>
2023-01-16 13:28:19 -0300

    lesosn 27

--- src/components/Lessons/Lessons.tsx
+++ src/components/Lessons/Lessons.tsx
@@ -520,6 +520,25 @@ return (
 </div>
 )
 }
+export const Lesson27 = () => {
+  const [answerSubmited, setAnswerSubmited] = useState('')
+
+return (
+  <div className="QuestionContainer">
+    <GetBack />
+{data.lessons.lesson27.map((item) =>  ( 
+      <div className="box" key={item.number}>
+        <h4>{item.question}</h4>
+        <div>
+
+        <input onChange={(e) => setAnswerSubmited(e.target.value)} />
+        <Button answerSubmited={answerSubmited} answer={item.answer} number={item.number}  />
+      </div>
+      </div>
+))}        
+</div>
+)
+}
 
 
 
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 import Navbar from '../Navbar/Navbar'
 import './styles.css'
 
-const lessons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]
+const lessons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27]
 
 const Main = () => {
   return (
